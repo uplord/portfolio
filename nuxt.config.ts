4b1ff488500e81a0068050986ad7d2416b7b0745
Michael <michael@uplord.co.uk>
2023-12-27 17:13:42 +0000

    remove security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,4 @@ export default defineNuxtConfig({
     buildAssetsDir: "/nuxt/",
   },
   modules: ['nuxt-icons', 'nuxt-security', "@nuxt/image"],
-  security: {
-    nonce: true,
-    headers: {
-      contentSecurityPolicy: {
-        'script-src': [
-          "'self'",
-          "https:",
-          "'unsafe-inline'",
-          "https://silly-genie-cd642a.netlify.app"
-        ],
-      }
-    },
-  }
 })
