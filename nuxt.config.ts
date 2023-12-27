5cbcf219449c81e61e023873954741e00799c4f9
Michael <michael@uplord.co.uk>
2023-12-27 16:55:13 +0000

    security amends

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,34 @@ export default defineNuxtConfig({
   },
   modules: ['nuxt-icons', 'nuxt-security', "@nuxt/image"],
   security: {
+    nonce: true,
+    ssg: {
+      hashScripts: true,
+      hashStyles: false
+    },
     headers: {
       contentSecurityPolicy: {
-        'script-src': ["'self'", 'data:', 'https://silly-genie-cd642a.netlify.app'],
-      },
+        'script-src': [
+          "'self'",
+          "https:",
+          "'unsafe-inline'",
+          "https://silly-genie-cd642a.netlify.app"
+        ],
+        'style-src': [
+          "'self'",
+          "https:",
+          "'unsafe-inline'"
+        ],
+        'img-src': ["'self'", "data:"],
+        'font-src': ["'self'", "https:", "data:"],
+        'base-uri': ["'none'"],
+        'object-src': ["'none'"],
+        'script-src-attr': ["'none'"],
+        'form-action': ["'self'"],
+        'frame-ancestors': ["'self'"],
+        'upgrade-insecure-requests': true
+      }
     },
-  },
+    sri: true
+  }
 })
