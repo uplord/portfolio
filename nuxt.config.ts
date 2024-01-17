e1bdaba4e2e4c47926657c0d7d99c1dcdd6d5f44
Michael <michael@uplord.co.uk>
2024-01-17 00:09:23 +0000

    nuxt security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
@@ -15,8 +16,41 @@ export default defineNuxtConfig({
       {
         fix: true
       }
-    ]
+    ],
+    'nuxt-security'
   ],
+  security: {
+    nonce: true, // Enables HTML nonce support in SSR mode
+    ssg: {
+      hashScripts: true, // Enables CSP hash support for scripts in SSG mode
+      hashStyles: false // Disables CSP hash support for styles in SSG mode (recommended)
+    },
+    headers: {
+      contentSecurityPolicy: {
+        'script-src': [
+          "'self'", // Fallback value, will be ignored by most modern browsers (level 3)
+          'https:', // Fallback value, will be ignored by most modern browsers (level 3)
+          "'unsafe-inline'", // Fallback value, will be ignored by almost any browser (level 2)
+          "'strict-dynamic'", // Strict CSP via 'strict-dynamic', supported by most modern browsers (level 3)
+          "'nonce-{{nonce}}'" // Enables CSP nonce support for scripts in SSR mode, supported by almost any browser (level 2)
+        ],
+        'style-src': [
+          "'self'", // Enables loading of stylesheets hosted on same origin
+          'https:', // For increased security, replace by the specific hosting domain or file name of your external stylesheets
+          "'unsafe-inline'" // Recommended default for most Nuxt apps
+        ],
+        'img-src': ["'self'", 'data:'], // Add relevant https://... sources if you load images from external sources
+        'font-src': ["'self'", 'https:', 'data:'], //  For increased security, replace by the specific sources for fonts
+        'base-uri': ["'none'"],
+        'object-src': ["'none'"],
+        'script-src-attr': ["'none'"],
+        'form-action': ["'self'"],
+        'frame-ancestors': false,
+        'upgrade-insecure-requests': true
+      }
+    },
+    sri: true
+  },
   googleFonts: {
     families: {
       'DM Sans': [400, 500, 600, 700]
