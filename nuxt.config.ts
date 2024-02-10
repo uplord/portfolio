8c82808a7781d3c253bb0255ba15b0e7aa5a0351
Michael <michael@uplord.co.uk>
2024-02-10 09:10:07 +0000

    move files into src folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/less/style.less"],
+  srcDir: "src/",
   ssr: false,
   devtools: { enabled: true },
   app: {
