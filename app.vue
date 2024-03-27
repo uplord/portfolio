2f5d4c56ae82c3fce97a4e48dd2fbdf4e5dae28d
Michael <michael@uplord.co.uk>
2024-03-27 13:56:28 +0000

    service worker

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
 <template>
   <NuxtLayout />
 </template>
+
+<script>
+import { defineComponent } from "@vue/composition-api"
+
+export default defineComponent({
+  setup() {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js")
+      })
+    }
+  }
+})
+</script>
