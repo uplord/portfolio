277227804181fe008902325ea2d80eec0c8df8d5
Michael <michael@uplord.co.uk>
2024-07-06 15:09:12 +0100

    Revert "test"
    This reverts commit 4cf2d7918668295b97bef47626c729d894630f62.

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,93 +1,17 @@
 <template>
-  <main>
-    <h1>DeviceMotion Event Demo</h1>
-    <p>Move your device to see the motion data.</p>
-    <div id="output">
-      Accelerometer: <br>
-      x: <span>{{ accelX }}</span> <br>
-      y: <span>{{ accelY }}</span> <br>
-      z: <span>{{ accelZ }}</span> <br>
-      <br>
-      Gyroscope: <br>
-      alpha: <span>{{ gyroAlpha }}</span> <br>
-      beta: <span>{{ gyroBeta }}</span> <br>
-      gamma: <span>{{ gyroGamma }}</span> <br>
-      <p v-if="errorMessage">{{ errorMessage }}</p>
-    </div>
-  </main>
+  <main>Test</main>
 </template>
 
 <script>
 import { ref, onMounted, onUnmounted } from 'vue';
 export default {
   setup() {
-    const accelX = ref('0');
-    const accelY = ref('0');
-    const accelZ = ref('0');
-    const gyroAlpha = ref('0');
-    const gyroBeta = ref('0');
-    const gyroGamma = ref('0');
-    const errorMessage = ref('');
-
-    function handleMotion(event) {
-      console.log(55)
-      if (event.acceleration) {
-        accelX.value = event.acceleration.x ? event.acceleration.x.toFixed(2) : 'N/A';
-        accelY.value = event.acceleration.y ? event.acceleration.y.toFixed(2) : 'N/A';
-        accelZ.value = event.acceleration.z ? event.acceleration.z.toFixed(2) : 'N/A';
+    useHead({
+      title: "Title",
+      htmlAttrs: {
+        lang: "en"
       }
-
-      if (event.rotationRate) {
-        gyroAlpha.value = event.rotationRate.alpha ? event.rotationRate.alpha.toFixed(2) : 'N/A';
-        gyroBeta.value = event.rotationRate.beta ? event.rotationRate.beta.toFixed(2) : 'N/A';
-        gyroGamma.value = event.rotationRate.gamma ? event.rotationRate.gamma.toFixed(2) : 'N/A';
-      }
-    }
-
-    function checkDeviceMotionPermission() {
-      console.log(1111)
-      if (typeof DeviceMotionEvent !== 'undefined') {
-        if (typeof DeviceMotionEvent.requestPermission === 'function') {
-          console.log(2222)
-          DeviceMotionEvent.requestPermission()
-            .then(permissionState => {
-              if (permissionState === 'granted') {
-                window.addEventListener('devicemotion', handleMotion);
-              } else {
-                errorMessage.value = 'Permission to access motion data was denied.';
-              }
-            })
-            .catch(err => {
-              console.error(err);
-              errorMessage.value = 'Error requesting permission for motion data.';
-            });
-        } else {
-          console.log(4444)
-          window.addEventListener('devicemotion', handleMotion);
-        }
-      } else {
-        console.log(3333)
-        errorMessage.value = 'DeviceMotionEvent is not supported on this device.';
-      }
-    }
-
-    onMounted(() => {
-      checkDeviceMotionPermission();
-    });
-
-    onUnmounted(() => {
-      window.removeEventListener('devicemotion', handleMotion);
-    });
-
-    return {
-      accelX,
-      accelY,
-      accelZ,
-      gyroAlpha,
-      gyroBeta,
-      gyroGamma,
-      errorMessage
-    };
+    })
   },
 }
 </script>
